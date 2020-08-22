8dac56aadc65549238685e8dba7434aa8081d838
Jurgen Schellaert <jurgen.schellaert@gmail.com>
2020-08-22 06:29:52 +0200

    corrected DocentRepository (removed @Repository annotation)

--- src/main/java/be/vdab/fietsacademy/repositories/DocentRepository.java
+++ src/main/java/be/vdab/fietsacademy/repositories/DocentRepository.java
@@ -4,6 +4,7 @@ import be.vdab.fietsacademy.domain.Docent;
 
 import java.util.Optional;
 
+// do not use @Repository annotation in the interface !!!!! Only in the implementing classes!!
 public interface DocentRepository {
 
     Optional<Docent> findById(long id);
