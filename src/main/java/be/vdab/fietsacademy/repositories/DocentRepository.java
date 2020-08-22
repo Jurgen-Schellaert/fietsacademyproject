6caf3de07b0d2fa006776f87adc64e71b034893a
Jurgen Schellaert <jurgen.schellaert@gmail.com>
2020-08-22 06:24:20 +0200

    added opslag() method to Docent entity and  created docent service to propagate the result of opslag() to the backing store.

--- src/main/java/be/vdab/fietsacademy/repositories/DocentRepository.java
+++ src/main/java/be/vdab/fietsacademy/repositories/DocentRepository.java
@@ -1,11 +1,9 @@
 package be.vdab.fietsacademy.repositories;
 
 import be.vdab.fietsacademy.domain.Docent;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface DocentRepository {
 
     Optional<Docent> findById(long id);
